19d7b0f0c061c1af27495956a320dacc1f4b1a58
apoorva patrikar <apoorvapatrikar.27@gmail.com>
2017-09-06 07:59:46 -0400

    Update hw.java

--- hw.java
+++ hw.java
@@ -1,7 +1,11 @@
-public class autoacn{
+public class hw{
   public static void main(String arg[]){
 
     System.out.println("Hello Jenkins, from github!");
+    System.out.println("Will print numbers now.....");
+    
+    for(int i=0; i<100; i+=7)
+      System.out.println(i);
 
   }
 }
