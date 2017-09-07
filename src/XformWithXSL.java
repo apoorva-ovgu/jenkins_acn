a34f1a81c68b84bb3e26099eae48f3a936d074e9
apoorva patrikar <apoorvapatrikar.27@gmail.com>
2017-09-07 10:19:18 -0400

    updated unit test

--- src/XformWithXSL.java
+++ src/XformWithXSL.java
@@ -1,6 +1,8 @@
 import java.io.*;
+import java.time.LocalDateTime;
 import javax.xml.transform.*;
 import javax.xml.transform.stream.*;
+
 
 public class XformWithXSL {    
 	public static void main(String args[]) {
@@ -10,7 +12,7 @@ public class XformWithXSL {
 		for (int i = 0; i < listOfFiles.length; i++) {
 		      if (listOfFiles[i].isFile() && (listOfFiles[i].getName().indexOf(".xml")==listOfFiles[i].getName().length()-4)) {
 		        System.out.println("Processing::=> " + "reports/"+ listOfFiles[i].getName());
-		        xsl("reports/"+listOfFiles[i].getName(),"menustyle.xsl", "reports/"+listOfFiles[i].getName()+"_transformed.html");
+		        xsl("reports/"+listOfFiles[i].getName(),"menustyle.xsl", "reports/"+listOfFiles[i].getName()+"_transformed.html",(i+1));
 		        
 		      } else if (listOfFiles[i].isDirectory()) {
 		        System.out.println("Directory found instead of XML file: " + listOfFiles[i].getName());
@@ -18,33 +20,65 @@ public class XformWithXSL {
 		    }
 	}
 	
-    public static void xsl(String xmlFile,  String styleFile, String transformedXml) {
-        try {
+    public static void xsl(String xmlFile,  String styleFile, String transformedXml, int indexOfXMLfile) {
+    	String statusOfOp = "";
+    	
+        try {        	        	
             // Create transformer factory and templates, and initialize transformer
             TransformerFactory factory = TransformerFactory.newInstance();
             Templates template = factory.newTemplates(new StreamSource(
                 new FileInputStream(styleFile)));            
             Transformer xformer = template.newTransformer();
-
            
             Source source = new StreamSource(new FileInputStream(xmlFile));
             Result result = new StreamResult(new FileOutputStream(transformedXml));            
             xformer.transform(source, result);
             
         } catch (FileNotFoundException e) {
-        	System.out.println("File unavailable at given location: "+e);
+        	statusOfOp = "FAILED TRANSFORMATION: File unavailable at given location: "+e;
         } catch (TransformerConfigurationException e) {
-        	System.out.println("TransformerConfigurationException: "+e);
-            // An error occurred in the XSL file
-        } catch (TransformerException e) {            
-            SourceLocator locator = e.getLocator();
-            int col = locator.getColumnNumber();
-            int line = locator.getLineNumber();
-            String publicId = locator.getPublicId();
-            String systemId = locator.getSystemId();
-            System.out.println("Error at transforming column "+col+" on line "+line +"\nPublicID "+publicId+" on system "+systemId);
+        	statusOfOp = "FAILED TRANSFORMATION: TransformerConfigurationException: "+e;        	
+        } catch (TransformerException e) {                       
+        	statusOfOp = "Error with XML format/XSL in file: "+xmlFile+"....... Details: "+e;
         }
-        
-        System.out.println("Done");
+        if (statusOfOp=="")
+        		statusOfOp = "Successful transformation";
+        checkResults(indexOfXMLfile, xmlFile, statusOfOp);
+    }
+    
+    public static void checkResults(int indexOfXMLfile, String inputxml, String statusOfOp) {
+        PrintWriter pw;
+		try {
+			String resultFile = "resultOfXSLT.csv"; 
+			String shortStatus = "";
+			if (statusOfOp == "Successful transformation")
+				shortStatus = "Success";
+			else
+				shortStatus = "FAILED TRANSFORMATION";
+			
+			boolean fileExists = true;
+			
+			File f1 = new File(resultFile);
+	         if(!f1.exists()) {
+	            f1.createNewFile();
+	            fileExists = false;
+	         } 
+	         FileWriter fileWritter = new FileWriter(f1.getName(),true);
+	         BufferedWriter bw = new BufferedWriter(fileWritter);
+	         
+	         if(!fileExists) {
+	        	 String header = ("srNo,Timestamp,XML file,Status,Expected Result,Current Result,\n");
+		         bw.write(header);
+	         }
+	         
+	         LocalDateTime t = LocalDateTime.now();
+
+		     bw.write(indexOfXMLfile+","+t+","+inputxml+","+shortStatus+",Successful transformation,"+statusOfOp+",\n");	   
+		        
+	         bw.close();
+	 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 }
